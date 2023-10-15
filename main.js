d024b0696387a5a2f769ef8e299fe557b24d94f8
Sina Fakhreddin <sina.fakhreddin@gmail.com>
2023-10-16 01:11:45 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+
+
+$(document).ready(function (e) {
+
+
+
+    $(".right").on("click" , function () {
+        let currentSlide = $(".first-slider-slide.active")
+        let next = currentSlide.next(".first-slider-slide")
+        if (next.length && next.css("opacity" , '0') ){
+            currentSlide.animate({"opacity":"0"} , 1500 , function () {
+                currentSlide.css("display" , "none").removeClass('active')
+               next.css("display" , "flex")
+                next.animate({"opacity":"1"},1500 , function (){
+                    next.addClass('active')
+                })
+            })
+        } else {
+                currentSlide.animate({"opacity":"0"},1500,function () {
+                    $(this).removeClass("active")
+                    currentSlide.css("display" , "none")
+                   $(".first-slider-slide ").first().css("display" , "flex").animate({"opacity":"1"} , 1500 , function () {
+                       $(this).addClass("active")
+                   })
+
+                })
+        }
+
+
+    })
+
+    $(".left").on("click" , function () {
+        let currentSlide = $(".first-slider-slide.active")
+        let prevSlide = currentSlide.prev().length === 0 ? $(".first-slider").children().eq(2) : currentSlide.prev()
+
+
+        currentSlide.animate({"opacity":"0"},1500 , function () {
+            currentSlide.css("display","none")
+            currentSlide.removeClass('active')
+            prevSlide.css("display","flex")
+            prevSlide.animate({"opacity" : "1"} , 1500 , function () {
+                prevSlide.addClass('active')
+            })
+
+        })
+
+
+    })
+
+    $(".navigator").click(function (e) {
+        let current = $(".second-slider .active")
+        let next = current.next()
+        if (next.length && next.css("opacity" , '0')){
+            current.animate({"opacity":"0"},1500 , function () {
+                current.css("display","none")
+                current.removeClass('active')
+                next.css("display","flex")
+                next.animate({"opacity" : "1"} , 1500 , function () {
+                next.addClass('active')
+                })
+
+            })
+        } else {
+            $('.second-slider .active').animate({"opacity":"0"},1500, function () {
+                $(".second-slider .active").css("display" , "none").removeClass("active")
+                $(".second-slide").first().css("display" , "flex").animate({"opacity":"1"},1500).addClass("active")
+
+            })
+
+
+        }
+
+    })
+
+    $(".responsive-slider-navigator").on("click",function () {
+        let current = $(".responsive-slider-card.active")
+        let next = current.next(".responsive-slider-card")
+        console.log(next.length , next)
+
+        if (next.length &&  next.css("opacity" , '0')){
+            current.animate({"opacity":"0"},1500 , function () {
+                current.css("display","none")
+                current.removeClass('active')
+                next.css("display","flex")
+                next.animate({"opacity" : "1"} , 1500 , function () {
+                    next.addClass('active')
+                })
+
+            })
+        } else {
+                current.animate({"opacity":"0"},1500, function () {
+                   current.css("display" , "none")
+                    current.removeClass("active")
+                    $(".responsive-slider-card").first().css("display" , "flex").animate({"opacity":"1"},1500,function () {
+                        $(this).addClass("active")
+                    })
+
+                })
+
+
+        }
+
+
+    })
+
+})
