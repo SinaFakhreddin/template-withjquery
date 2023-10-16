506a834f72b826d00e909bfa92d4235fd81c4652
Sina Fakhreddin <sina.fakhreddin@gmail.com>
2023-10-16 15:14:46 +0330

    :)

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 
 
 $(document).ready(function (e) {
+    let counter =1
     let totalSlides = $(".first-slider .first-slider-slide").length
     $(".total-of-slides").text(totalSlides)
+    $(".current-slide").text(counter)
+
+    $(".first-slider.first-slider-slide.active")
 
     $('.first-slider .first-slider-slide').each(function(index) {
         $(this).attr('data-slide-index', index);
     });
 
-    let   activeSlideIndex = $('.first-slider-slide.active').attr('data-slide-index');
-    $('.slider-counter').text(activeSlideIndex);
 
     $(".right").on("click" , function () {
+
         let currentSlide = $(".first-slider-slide.active")
         let next = currentSlide.next(".first-slider-slide")
         if (next.length && next.css("opacity" , '0') ){
@@ -20,6 +23,13 @@ $(document).ready(function (e) {
                next.css("display" , "flex")
                 next.animate({"opacity":"1"},1500 , function (){
                     next.addClass('active')
+                    counter +=1
+                    console.log(counter)
+                    if (counter==4 || counter<4){
+                        $(".current-slide").text(counter)
+                    } else {
+                        $(".current-slide").text(counter=1)
+                    }
                 })
             })
         } else {
@@ -39,14 +49,14 @@ $(document).ready(function (e) {
     $(".left").on("click" , function () {
         let currentSlide = $(".first-slider-slide.active")
         let prevSlide = currentSlide.prev().length === 0 ? $(".first-slider").children().eq(2) : currentSlide.prev()
-
-
         currentSlide.animate({"opacity":"0"},1500 , function () {
             currentSlide.css("display","none")
             currentSlide.removeClass('active')
             prevSlide.css("display","flex")
             prevSlide.animate({"opacity" : "1"} , 1500 , function () {
                 prevSlide.addClass('active')
+                $(".current-slide").text($(".first-slider .active").attr("datafld"))
+
             })
 
         })
